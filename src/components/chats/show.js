a3aace4416e32e99334f0bc4d5169620a1e972ef
JP <bonnerjp@gmail.com>
2016-05-19 20:46:13 -0400

    ip

--- src/components/chats/show.js
+++ src/components/chats/show.js
@@ -1,65 +1,142 @@
 import React, { Component } from 'react'
 import {
-  AppRegistry,
+  Dimensions,
   StyleSheet,
   Text,
   TextInput,
   ListView,
   View
 } from 'react-native'
+import GiftedMessenger from 'react-native-gifted-messenger'
 
-import ChatView from '../util/chatview'
+export default class ChatsShow extends Component {
+  constructor(props) {
+    super(props);
+  
+    this._messages = this.getInitialMessages();
 
-var ChatsShow = React.createClass({
-  getInitialState() {
-    var items = new ListView.DataSource({
-      rowHasChanged: (r1, r2) => r1.name !== r2.name || r1.content !== r2.content,
-    })
+    this.state = {
+      messages: this._messages, // not a listview datasource!
+      isLoadingEarlierMessages: false,
+      typingMessage: '',
+      allLoaded: false,
+    };
+  }
 
-    return {
-      items: items.cloneWithRows(seeds),
-      _items: seeds
-    }
+  getInitialMessages() {
+    // @todo GET FB
+    return [
+      {
+        text: 'The Tent lives!',
+        name: 'React-Bot',
+        image: {uri: 'https://facebook.github.io/react/img/logo_og.png'},
+        position: 'left',
+        date: new Date(2016, 3, 14, 13, 0),
+        uniqueId: Math.round(Math.random() * 10000), // simulating server-side unique id generation
+      },
+      {
+        text: "Drop the 'the', it's already been lampshaded",
+        name: 'Awesome Developer',
+        image: { uri: 'http://0.gravatar.com/avatar/fa4d287d26d7568219b3af5f268eb394' },
+        position: 'right',
+        date: new Date(2016, 3, 14, 13, 1),
+        uniqueId: Math.round(Math.random() * 10000), // simulating server-side unique id generation
+      },
+    ];
+  }
 
-  },
+  setMessages(messages) {
+    this._messages = messages;
+    this.setState({ messages: messages });
+  }
 
-  _handleTextChange(e) {
-    this.state._items = this.state._items.concat({
-      name: 'You', content: e.nativeEvent.text
-    })
+  handleSend(message = {}) {
+    // @todo POST FB
+
+    // Your logic here
+    // Send message.text to your server
+
+    message.uniqueId = Math.round(Math.random() * 10000); // simulating server-side unique id generation
+    this.setMessages(this._messages.concat(message));
+
+    // mark the sent message as Seen
+    setTimeout(() => {
+      this.setMessageStatus(message.uniqueId, 'Seen'); // here you can replace 'Seen' by any string you want
+    }, 1000);
+
+    // if you couldn't send the message to your server :
+    // this.setMessageStatus(message.uniqueId, 'ErrorButton');
+  }
+
+  onLoadEarlierMessages() {
+    // @todo GET FB/<page-1>
+
+    // display a loader until you retrieve the messages from your server
     this.setState({
-      items: this.state.items.cloneWithRows(this.state._items)
-    })
-  },
+      isLoadingEarlierMessages: true,
+    });
 
-  item(item) {
-    return (
-      <View style={styles.item}>
-        <Text style={styles.name}>{item.name}: </Text>
-        <Text>{item.content}</Text>
-      </View>
-    )
-  },
+    // Your logic here
+    // Eg: Retrieve old messages from your server
+
+    // IMPORTANT
+    // Oldest messages have to be at the begining of the array
+    var earlierMessages = [
+      {
+        text: 'React Native enables you to build world-class application experiences on native platforms using a consistent developer experience based on JavaScript and React. https://github.com/facebook/react-native',
+        name: 'React-Bot',
+        image: {uri: 'https://facebook.github.io/react/img/logo_og.png'},
+        position: 'left',
+        date: new Date(2016, 0, 1, 20, 0),
+        uniqueId: Math.round(Math.random() * 10000), // simulating server-side unique id generation
+      }, {
+        text: 'This is a touchable phone number 0606060606 parsed by taskrabbit/react-native-parsed-text',
+        name: 'Awesome Developer',
+        image: null,
+        position: 'right',
+        date: new Date(2016, 0, 2, 12, 0),
+        uniqueId: Math.round(Math.random() * 10000), // simulating server-side unique id generation
+      },
+    ];
+
+    setTimeout(() => {
+      this.setMessages(earlierMessages.concat(this._messages)); // prepend the earlier messages to your list
+      this.setState({
+        isLoadingEarlierMessages: false, // hide the loader
+        allLoaded: true, // hide the `Load earlier messages` button
+      });
+    }, 1000); // simulating network
+  }
+
+  handleReceive(message = {}) {
+    // @todo point FB listener here
+
+    // REQ PARAMS:
+    // text, name, image, position: 'left', date, uniqueId
+    this.setMessages(this._messages.concat(message));
+  }
 
   render() {
     return (
-      <View>
-        <Text>Chats: show</Text>
-        <ChatView
-          style={styles.chat}
-          dataSource={this.state.items}
-          renderRow={this.item}
-          initialListSize={this.state.items.length}
-        />
-        <TextInput
-          style={styles.input}
-          returnKeyType='go'
-          onSubmitEditing={this._handleTextChange}
-        />
-      </View>
+      <GiftedMessenger
+        ref={(c) => this._GiftedMessenger = c}
+        styles={styles.bubbleRight}
+        autoFocus={true}
+        messages={this.state.messages}
+        handleSend={this.handleSend.bind(this)}
+        maxHeight={400}
+        loadEarlierMessagesButton={!this.state.allLoaded}
+        onLoadEarlierMessages={this.onLoadEarlierMessages.bind(this)}
+        senderName='JP'
+        senderImage={{url: 'http://0.gravatar.com/avatar/fa4d287d26d7568219b3af5f268eb394'}}
+        displayNames={true}
+        parseText={false}
+        isLoadingEarlierMessages={this.state.isLoadingEarlierMessages}
+        typingMessage={this.state.typingMessage}
+      />
     )
-  },
-})
+  }
+}
 
 const styles = StyleSheet.create({
   item: {
@@ -71,6 +148,10 @@ const styles = StyleSheet.create({
   },
   chat: {
     height: 300
+  },
+  bubbleRight: {
+    marginLeft: 70,
+    backgroundColor: '#116611',
   },
   input: {
     backgroundColor: '#ffffff',
